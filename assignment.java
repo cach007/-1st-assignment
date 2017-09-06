602ea63ab2face41c4d8439ab0e7493455bce3c6
cach007 <31638904+cach007@users.noreply.github.com>
2017-09-06 12:01:50 +0900

    Add files via upload
    1st

---
+++ assignment.java
@@ -0,0 +1,12 @@
+
+public class assignment {
+
+	public static void main(String[] args) {
+		System.out.println("1. 학번=2016301038");
+		System.out.println("2. 이름=박진홍");
+		System.out.println("3. phone number=01020208978");
+		System.out.println("4. github address=github.com/cach007");
+
+	}
+
+}
